54c72b251952320b1f29262e61ed689c506025b4
vanpipy <vanpipy@gmail.com>
2018-06-28 23:49:40 +0800

    Add a taste code of promise, it's not good but easy to understand.

--- promise.js
+++ promise.js
@@ -1,11 +1,59 @@
 
-const _$ = require('compose-parallel');
+// const { curry, compose } = require('compose-parallel');
 
-(function (global) {
-    'use strict';
-    
-    function Promise () {
-        
+const stages = { pending: 0, fulfilled: 1, rejected: 2, };
+
+function noop () {}
+
+/*
+ * 1. The value.
+ * 2. Task queue.
+ */
+function Promise (fn) {
+    const ctx = this;
+
+    ctx.value = void 0;
+    ctx.stage = stages.pending;
+
+    fn(function(value) {
+        ctx.resolve(value);
+    }, function(reason) {
+        ctx.reject(reason);
+    });
+}
+
+Promise.prototype.toString = () => '[object Promise]';
+
+Promise.prototype.resolve = function (value) {
+    //Unfold the input
+    this.stage = stages.fulfilled;
+
+    if (this.next) {
+        this.next.value = this.onFulfilled(value);
+        this.next.resolve(this.next.value);
     }
-    
-})(window);
+};
+
+Promise.prototype.reject = function (reason) {
+
+};
+
+Promise.prototype.then = function (onFulfilled, onRejected) {
+    this.onFulfilled = onFulfilled;
+    this.onRejected = onRejected;
+    this.next = new Promise(noop);
+
+    return this.next;
+};
+
+const t = new Promise(function(resolve, reject) {
+    setTimeout(() => {
+        resolve(100);
+    }, 1000);
+});
+
+t.then((result) => {
+    return result * 2;
+}).then((r) => {
+    console.log(r);
+});
